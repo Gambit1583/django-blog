563d34300dc9dc8598e8324992b3124ccde2672f
Paul Jebb <pjebb15@gmail.com>
2024-11-15 12:24:59 +0000

    Added functionality to admin panel and fixtures to blog app

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =  os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-gambit1583-djangoblog-p5esrqo7fvw.ws.codeinstitute-ide.net", "8000-gambit1583-djangoblog-p5esrqo7fvw.ws.codeinstitute-ide.net", '.herokuapp.com',]
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
